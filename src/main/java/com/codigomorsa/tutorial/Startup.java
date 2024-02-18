452bb90312bc36f7c136e801f0d9b3506256cd35
ealmonte <elvisalmonte14@gmail.com>
2024-02-18 14:42:57 -0400

    uso de la clase Logger

---
+++ src/main/java/com/codigomorsa/tutorial/Startup.java
@@ -0,0 +1,20 @@
+package com.codigomorsa.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+	private static Logger logger = LoggerFactory.getLogger(Startup.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+		logger.debug("mi mensaje debug");
+		logger.info("mi mensaje info");
+		logger.warn("mi mensaje warn");
+		logger.error("mi mensaje error");
+	}
+
+}
